e805b4bb0c07956b29cb69f3492206b2bd40d95d
Sead Memic <sead.memic@intracto.com>
2023-10-03 20:22:40 +0200

    memory leak changes

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-app>
+        <v-main>
+            <v-alert>
+                Memory leak test
+            </v-alert>
+            <v-btn>Test</v-btn>
+        </v-main>
+    </v-app>
+</template>
